70f308b652de3718495f234a7be142c0b0805d15
Katya Casasola <50116154+kccasasola@users.noreply.github.com>
2026-09-10 22:40:12 -0300

    form fixed

--- app/components/Reserva.tsx
+++ app/components/Reserva.tsx
@@ -168,7 +168,14 @@ export default function Reserva() {
   );
 }*/
 
-<iframe data-tally-src="https://tally.so/embed/lb1516?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="675" frameborder="0" marginheight="0" marginwidth="0" title="Reservar mi lugar"></iframe>
-<script>
-    var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
-</script>
+<div className="w-full">
+  <iframe
+    src="https://tally.so/embed/lb1516?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+    width="100%"
+    height="700"
+    frameBorder="0"
+    marginHeight={0}
+    marginWidth={0}
+    title="Confirmación de asistencia"
+  />
+</div>
